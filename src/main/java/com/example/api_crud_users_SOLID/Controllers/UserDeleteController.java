414a3d11ebe22c5c0c9ecd191844f4eb077e06fd
sergigarcia32 <sergigarciaDev@outlook.com>
2026-08-18 20:04:36 +0200

    delete imports

--- src/main/java/com/example/api_crud_users_SOLID/Controllers/UserDeleteController.java
+++ src/main/java/com/example/api_crud_users_SOLID/Controllers/UserDeleteController.java
@@ -1,10 +1,7 @@
 package com.example.api_crud_users_SOLID.Controllers;
 
-import com.example.api_crud_users_SOLID.Exception.EmailAlreadyExistsException;
-import com.example.api_crud_users_SOLID.Services.UserCreator;
 import com.example.api_crud_users_SOLID.Services.UserRemover;
 import com.example.api_crud_users_SOLID.dto.ErrorResponse;
-import com.example.api_crud_users_SOLID.dto.UserRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
